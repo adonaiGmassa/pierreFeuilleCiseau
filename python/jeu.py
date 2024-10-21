beff5d0a03d0e369456616f403f2448c08dfd6be
Massa Adonaï <167198345+adonaiGmassa@users.noreply.github.com>
2024-10-21 17:01:02 +0200

    modif py #ajout variable

--- python/jeu.py
+++ python/jeu.py
@@ -2,3 +2,15 @@
 import random
 import sys
 import pygame
+
+# Initialisation de Pygame
+pygame.init()
+
+# Dimensions de la fenêtre
+largeur, hauteur = 800, 600
+ecran = pygame.display.set_mode((largeur, hauteur))
+pygame.display.set_caption("Pierre, Feuille, Ciseau")
+
+# Couleurs
+blanc = (255, 255, 255)
+noir = (0, 0, 0)
