600d1a3b36c52c6d7822d4a06b7698ca4c08e324
Massa Adonaï <massaadonai@gmail.com>
2024-11-26 22:16:46 +0100

    ajout variable jeu

---
+++ Csharp/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    public string[] choices = { "Pierre", "Feuille", "Ciseau" };
+    Random rand = new Random();
+    // Choix de l'ordinateur
+    int computerChoiceIndex = rand.Next(choices.Length);
+    string computerChoice = choices[computerChoiceIndex];
+
+    // Fonction pour mettre la première lettre en majuscule
+    static string CapitalizeFirstLetter(string input)
+    {
+        return char.ToUpper(input[0]) + input.Substring(1);
+    }
+        
+    
+}
