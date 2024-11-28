4d081b5469f6006aeefc970e9e0868ed142af641
Massa Adonaï <167198345+adonaiGmassa@users.noreply.github.com>
2024-11-28 20:04:06 +0100

    Modif variable

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -2,23 +2,24 @@ using System;
 
 class Program
 {
-    public string[] choices = { "Pierre", "Feuille", "Ciseau" };
+    #region Variables
+    // Choix possible 
+    public string[] choix = { "Pierre", "Feuille", "Ciseau" };
     Random rand = new Random();
     // Choix de l'ordinateur
-    int computerChoiceIndex = rand.Next(choices.Length);
-    string computerChoice = choices[computerChoiceIndex];
+    static int choixOrdiIndex = rand.Next(Choix.Length);
+    static string choixOrdi = Choix[choixOrdiIndex];
 
     // Fonction pour mettre la première lettre en majuscule
     static string CapitalizeFirstLetter(string input)
     {
         return char.ToUpper(input[0]) + input.Substring(1);
     }
+    #endregion
         
     
 }
-
-
-    static void Main()
+static void Main()
     {
 
     }
